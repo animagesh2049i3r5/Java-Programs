ecbd6fbbfadc6825e98f8c30ee6e7ec92378089d
animagesh2049i3r5 <164443223+animagesh2049i3r5@users.noreply.github.com>
2026-06-03 16:08:45 -0700

    Create Program3.java

---
+++ Program3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args) {
+  
+    Scanner scanner = new Scanner(System.in);
+    String username;
+    
+    System.out.print("Enter your new username");
+    username = scanner.nextLine();
+    
+    if(username.length() < 4 || username.length() >12 ){
+      System.out.println("Username must be between 4-12 characters");
+    }
+    else if(username.contains(" ") || username.contains("_")){
+      System.out.println("Username must not containt spaces or underscores");
+    }
+    else{
+      System.out.println("Welcome "+username);
+    }
+    
+    scanner.close();
+  }
+}
